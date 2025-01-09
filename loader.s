ddfb4a03e5ddd18b87bff80b51ee1d877e0e50f9
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2025-01-09 14:48:54 +0300

    setting up a stack

--- loader.s
+++ loader.s
@@ -11,6 +11,13 @@ align 4                        ; the code must be4 byte aligned
     dd FLAGS                   ; the flags,
     dd CHECKSUM                ; and the checksum
 
+KERNEL_STACK_SIZE equ 4096     ; size of stack in bytes
+
+section .bss
+align 4                        ; align at 4 bytes
+kernel_stack:                  ; label points to beginning of memory
+    resb KERNEL_STACK_SIZE     ; reserve stack for the kernel
+
 loader:                        ; the loader label (defined as entry point in linker script)
     mov eax, 0xCAFEBABE        ; place the number 0xCAFEBABE in the register eax
 .loop:
